e3a0114df8205337c516ce4e242dec8a6245fff6
João Pedro Holanda <jpholanda.prf@gmail.com>
2024-06-05 21:10:06 -0300

    Change --dev argument to optional flag

--- baselines/trainer_config.py
+++ baselines/trainer_config.py
@@ -6,9 +6,13 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='model trainer')
     parser.add_argument('--dev',
                         dest='dev',
-                        type=bool,
-                        default=False,
-                        action=argparse.BooleanOptionalAction,
-                        help='enables dev mode (runs only one step to check if everything\'s working)')
+                        action='store_true',
+                        help='enables dev mode (runs only one step)')
+    parser.add_argument('--prod',
+                        dest='dev',
+                        action='store_false',
+                        help='do not enable dev mode (runs normally)')
+    parser.set_defaults(dev=False)
+
     args = parser.parse_args()
     return args
