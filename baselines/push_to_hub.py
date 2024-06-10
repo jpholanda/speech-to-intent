e591ee20b46e24c8459f257f0c25a474dc610f50
João Pedro Holanda <jpholanda.prf@gmail.com>
2024-06-10 16:55:32 -0300

    Add --model option to push to hub script

--- baselines/push_to_hub.py
+++ baselines/push_to_hub.py
@@ -1,11 +1,20 @@
-from trainer_config import get_arguments
 from trainer_hubert import LightningModel as HubertModel
+from trainer_wav2vec2 import LightningModel as Wav2VecModel
+from trainer_whisper import LightningModel as WhisperModel
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='push to hub')
+    parser.add_argument('--model', dest='model', choices=['hubert', 'wav2vec2', 'whisper'],
+                        help='model to upload', required=True)
     parser.add_argument('--checkpoint', dest='checkpoint', help='checkpoint to load from', required=True)
     args = parser.parse_args()
 
-    model = HubertModel.load_from_checkpoint(args.checkpoint)
-    model.push_to_hub("speech-to-intent_hubert")
+    if args.model == 'hubert':
+        model = HubertModel.load_from_checkpoint(args.checkpoint)
+    elif args.model == 'wav2vec2':
+        model = Wav2VecModel.load_from_checkpoint(args.checkpoint)
+    else:
+        model = WhisperModel.load_from_checkpoint(args.checkpoint)
+
+    model.push_to_hub(f"speech-to-intent_{args.model}")
